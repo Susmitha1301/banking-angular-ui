0315e8be30977f760be2af8ce3ac677143c7a666
Susmitha <susmitha.dev48@gmail.com>
2026-05-08 10:07:00 -0400

    Routing and Layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { CustomerListComponent } from './features/customers/customer-list/customer-list';
+import { AddCustomerComponent } from './features/customers/add-customer/add-customer';
+
+import { CustomerAccountsComponent } from './features/accounts/customer-accounts/customer-accounts';
+import { CreateAccountComponent } from './features/accounts/create-account/create-account';
+
+import { DepositComponent } from './features/transactions/deposit/deposit';
+import { WithdrawComponent } from './features/transactions/withdraw/withdraw';
+import { TransferComponent } from './features/transactions/transfer/transfer';
+import { TransactionHistoryComponent } from './features/transactions/transaction-history/transaction-history';
+
+export const routes: Routes = [
+
+  { path: 'customers', component: CustomerListComponent },
+
+  { path: 'add-customer', component: AddCustomerComponent },
+
+  { path: 'accounts', component: CustomerAccountsComponent },
+
+  { path: 'create-account', component: CreateAccountComponent },
+
+  { path: 'deposit', component: DepositComponent },
+
+  { path: 'withdraw', component: WithdrawComponent },
+
+  { path: 'transfer', component: TransferComponent },
+
+  { path: 'transactions', component: TransactionHistoryComponent },
+
+  { path: '', redirectTo: 'customers', pathMatch: 'full' }
+
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import { NavbarComponent } from './layout/navbar/navbar';
+import { SidebarComponent } from './layout/sidebar/sidebar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, NavbarComponent, SidebarComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('banking-angular-ui');
 }
